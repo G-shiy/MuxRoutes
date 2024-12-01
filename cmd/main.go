12ab28108315a4038d4ed02133e0c021ef9d07f6
gustavo <siqgustavo2@hotmail.com>
2024-12-01 14:52:43 -0300

    test database

--- cmd/main.go
+++ cmd/main.go
@@ -12,27 +12,18 @@ import (
 )
 
 func main() {
-	configEnv()
 
-	host := os.Getenv("DB_HOST")
-	port := os.Getenv("DB_PORT")
-	user := os.Getenv("DB_USER")
-	password := os.Getenv("DB_PASSWORD")
-	dbname := os.Getenv("DB_NAME")
+	db := configDb()
+	fmt.Println(db)
 
-	stringConexao := "host=" + host + " port=" + port + " user=" + user + " password=" + password + " dbname=" + dbname + " sslmode=disable"
-	db, err := sql.Open("postgres", stringConexao)
+	col, err := db.Query("SELECT * FROM users")
+
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal("erro ao executar a query")
 	}
+	fmt.Println(col)
 
 	defer db.Close()
-
-	if err := db.Ping(); err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Println(db)
 
 }
 
@@ -49,4 +40,32 @@ func configEnv() {
 		log.Fatal(err)
 	}
 
+}
+
+func configDb() *sql.DB {
+	configEnv()
+
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	dbname := os.Getenv("DB_NAME")
+
+	connStr := fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname,
+	)
+	fmt.Printf("Conectando ao banco de dados com: %s\n", connStr)
+
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatalf("Erro ao abrir conexão com o banco: %v", err)
+	}
+
+	if err := db.Ping(); err != nil {
+		log.Fatalf("Erro ao testar conexão com o banco: %v", err)
+	}
+	fmt.Println("Conexão estabelecida com sucesso!")
+
+	return db
 }
